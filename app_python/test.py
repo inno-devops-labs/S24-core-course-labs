61ff06dbd21f5763c9f7588d88670a1fe8833c04
pptx704 <rafeedm.bhuiyan@gmail.com>
2024-02-03 16:47:50 +0300

    Completed app description

--- app_python/test.py
+++ app_python/test.py
@@ -1,9 +1,21 @@
+"""
+This file contains the unit tests for the app_utils.py file.
+"""
+
 import unittest
 from app_python.app_utils import return_tz_time, return_time
 
 
 class TestAppUtils(unittest.TestCase):
+    """
+    Test cases for the app_utils.py file.
+    """
+
     def test_return_tz_time(self):
+        """
+        Tests the return_tz_time function.
+        It checks for different timezones and compares the results.
+        """
         # Istanbul and Moscow time will be same if return_tz_time is working correctly
         self.assertEqual(
             return_tz_time("Europe/Istanbul"), return_tz_time("Europe/Moscow")
@@ -14,7 +26,10 @@ class TestAppUtils(unittest.TestCase):
         )
 
     def test_return_time(self):
-        # Current Moscow time will be in the current time string
+        """
+        Tests the return_time function.
+        It checks if the current Moscow time is in the current time string.
+        """
         self.assertIn(return_tz_time("Europe/Moscow"), return_time())
 
 
