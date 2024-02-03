34c57002caaa4a6e98228622f5d9809456bc915b
pptx704 <rafeedm.bhuiyan@gmail.com>
2024-02-03 16:47:50 +0300

    Completed app description

--- app_python/app.py
+++ app_python/app.py
@@ -1,3 +1,7 @@
+"""
+Main application file. Contains the route information and necessary setups.
+"""
+
 from flask import Flask
 from app_python.app_utils import return_time
 
@@ -6,6 +10,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def show_time():
+    """
+    Index page. Shows the current time in Moscow.
+    """
     return return_time("Europe/Moscow")
 
 
--- app_python/app_utils.py
+++ app_python/app_utils.py
@@ -1,14 +1,26 @@
-import pytz
+"""
+Utility functions for the app. Can also be considered as the logic section.
+"""
 
 from datetime import datetime
-from flask import url_for
+import pytz
 
 
 def return_time(timezone: str = "Europe/Moscow") -> str:
+    """
+    Returns the current time in the given timezone.
+    :param timezone: The timezone to get the current time from. (Optional, default: Europe/Moscow)
+    :return: The current time in the given timezone.
+    """
     return f"""
     <h1>Current time in Moscow: {return_tz_time(timezone)}</h1>
     """
 
 
 def return_tz_time(timezone) -> str:
+    """
+    Returns the current time in the given timezone.
+    :param timezone: The timezone to get the current time from.
+    :return: The current time in the given timezone.
+    """
     return datetime.now(pytz.timezone(timezone)).strftime("%H:%M (%I:%M %p)")
