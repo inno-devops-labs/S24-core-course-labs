c898cf8d44fd0024e13a60b4a10fb54986f6a032
Yegor Blinov <39417909+blinikar@users.noreply.github.com>
2024-02-04 14:04:09 +0000

    lab1

---
+++ app_python/app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+import datetime
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def template():
+    return render_template(
+        'index.html', 
+        time=datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=3)))
+    )
+
+if __name__ == '__main__':
+    app.run()
