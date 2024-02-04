f9d1172706083c4366e5321aff2c5292bb7f52b3
IlnurHA <53339563+IlnurHA@users.noreply.github.com>
2024-02-04 18:37:33 +0300

    Removed redundant tag

--- app_python/api/v1/__init__.py
+++ app_python/api/v1/__init__.py
@@ -2,6 +2,6 @@ from fastapi import APIRouter
 
 from .time import router as time_router
 
-router = APIRouter(tags=["v1"])
+router = APIRouter()
 
 router.include_router(router=time_router, prefix="/time")
