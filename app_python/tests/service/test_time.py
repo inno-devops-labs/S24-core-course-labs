74fe884e91b9ae22a6e58d0e0e2d14a58dcd2f32
ADari-Ka <ADari.Ka@yandex.ru>
2024-02-05 21:00:44 +0300

    add: simple tests

---
+++ app_python/tests/service/test_time.py
@@ -0,0 +1,28 @@
+import asyncio
+
+import pytest
+from app.api import app, di
+from fastapi.testclient import TestClient
+
+
+@pytest.fixture()
+async def app_mock(time_manager_factory) -> TestClient:
+    client = TestClient(app)
+    app.dependency_overrides[di.time_manager] = time_manager_factory
+    return client
+
+
+@pytest.mark.asyncio
+async def test_current_time_moscow_update(app_mock):
+    app = await app_mock
+
+    resp1 = app.get("/")
+    assert resp1.status_code == 200
+
+    data = resp1.text
+
+    await asyncio.sleep(1)
+
+    resp2 = app.get("/")
+    assert resp2.status_code == 200
+    assert resp2.text != data
