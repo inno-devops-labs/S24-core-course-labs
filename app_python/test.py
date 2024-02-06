63681022a376c0b370ec56faebbc65b9f4977f29
Yegor Blinov <39417909+blinikar@users.noreply.github.com>
2024-02-06 09:49:55 +0000

    Lab1 fix

--- app_python/test.py
+++ app_python/test.py
@@ -1,6 +1,7 @@
 import unittest
 from app import app
 import datetime
+import time
 
 class TestFlaskApp(unittest.TestCase):
     def setUp(self):
@@ -10,7 +11,11 @@ class TestFlaskApp(unittest.TestCase):
     def test_display_time(self):
         response = self.app.get('/')
         self.assertEqual(response.status_code, 200)
-        self.assertIn(bytes(datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=3))).strftime('%d.%m.%y %H:%M'), 'utf-8'), response.data)
+        self.assertIn(bytes(datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=3))).strftime('%d.%m.%y %H:%M:%S'), 'utf-8'), response.data)
+        time.sleep(2)
+        response = self.app.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(bytes(datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=3))).strftime('%d.%m.%y %H:%M:%S'), 'utf-8'), response.data)
 
 if __name__ == '__main__':
     unittest.main()
