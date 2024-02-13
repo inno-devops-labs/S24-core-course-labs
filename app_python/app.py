f44019b2b7ecb1791814b7fc42afa9803c7acf81
LaithAlebrahim <l.alebrahim@innopolis.university>
2024-02-14 00:22:18 +0300

    Solved Task1+Task2

--- app_python/app.py
+++ app_python/app.py
@@ -11,4 +11,4 @@ def show_time():
     return render_template('time_template.html', time_now=time_now)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
