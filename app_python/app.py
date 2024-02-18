575758a449773c8c708683510f33809c969f4180
Vladimir Ryabenko <112640157+profectus200@users.noreply.github.com>
2024-02-18 19:51:50 +0300

    Update app.py

--- app_python/app.py
+++ app_python/app.py
@@ -5,11 +5,13 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    moscow_time = datetime.utcnow().replace(tzinfo=timezone.utc) + timedelta(hours=3)
-    return render_template('index.html', time=moscow_time.strftime('%Y-%m-%d %H:%M:%S'))
+    moscow_time = (datetime.utcnow().replace(tzinfo=timezone.utc) +
+                   timedelta(hours=3))
+    return render_template("index.html",
+                           time=moscow_time.strftime("%Y-%m-%d %H:%M:%S"))
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
