eea13a617f3948d184d2529cfbe905d5f9988a96
Abu Huraira <abuhuraira.dt@gmail.com>
2024-02-20 23:17:00 +0300

    add: task1 (set up github actions CI)

--- app_python/test_app.py
+++ app_python/test_app.py
@@ -1,12 +1,13 @@
 from .app import app, get_moscow_time
 import re
 
+
 def test_get_moscow_time_format():
-    
+
     moscow_time = get_moscow_time()
     # Check if the returned time matches the format "YYYY.MM.DD HH:MM:SS"
     assert re.match(r"\d{4}\.\d{2}\.\d{2} \d{2}:\d{2}:\d{2}", moscow_time)
-    
+
 
 def test_home_page():
     """
