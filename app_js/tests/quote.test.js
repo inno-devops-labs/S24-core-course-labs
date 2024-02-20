c8ff7e0c874a1363d057b113ea1c71e8bd63941c
LaithAlebrahim <l.alebrahim@innopolis.university>
2024-02-20 21:45:53 +0300

    Tested action for bonus task app_js

--- app_js/tests/quote.test.js
+++ app_js/tests/quote.test.js
@@ -21,3 +21,4 @@ describe('GET /', () => {
     expect(response.text).toMatch(/Life is about making an impact, not making an income. –Kevin Kruse|Whatever the mind of man can conceive and believe, it can achieve. –Napoleon Hill|Strive not to be a success, but rather to be of value. –Albert Einstein|Two roads diverged in a wood, and I—I took the one less traveled by, And that has made all the difference. –Robert Frost|The most difficult thing is the decision to act, the rest is merely tenacity. –Amelia Earhart|Every strike brings me closer to the next home run. –Babe Ruth|Definiteness of purpose is the starting point of all achievement. –W. Clement Stone|Life isn't about getting and having, it's about giving and being. –Kevin Kruse|Life is what happens to you while you’re busy making other plans. –John Lennon/);
   });
 });
+
