e5535abb6eadf253666da5a1482f565495a9d7aa
Alexandra Chupkova <a.chupkova@innopolis.university>
2024-02-20 21:22:28 +0300

    Lab 3

--- app_python/testing.py
+++ app_python/testing.py
@@ -1,5 +1,7 @@
 import unittest
+import re
 from main import app
+
 
 class TestFlaskApp(unittest.TestCase):
     def setUp(self):
@@ -9,7 +11,9 @@ class TestFlaskApp(unittest.TestCase):
     def test_display_time(self):
         response = self.app.get('/')
         self.assertEqual(response.status_code, 200)
-        self.assertIn(b'Current Time in Moscow', response.data)
+        time_string_regex = r'\d{2}:\d{2}:\d{2}'
+        self.assertTrue(re.search(time_string_regex, str(response.data)))
+
 
 if __name__ == '__main__':
     unittest.main()
