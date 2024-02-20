e47d705039b42222b20b12f63456c1e9999f888a
Ilnar <4sitescarp@gmail.com>
2024-02-20 23:38:39 +0300

    Fix file

--- app_python/src/business_logic.py
+++ app_python/src/business_logic.py
@@ -8,7 +8,6 @@ def get_current_moscow_time() -> datetime:
     return datetime.now(moscow_tz)
 
 
-
 def get_human_readable_time(dt: datetime) -> str:
     human_readable = dt.strftime("%a, %b %d %Y, %H:%M:%S")
     return human_readable
