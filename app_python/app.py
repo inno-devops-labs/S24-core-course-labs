feae3d64aea986705f302372bcc78786a434c907
mosta <mostafakhaled835@outlook.com>
2024-02-20 10:09:51 +0300

    Linting Text

--- app_python/app.py
+++ app_python/app.py
@@ -1,18 +1,21 @@
 from flask import Flask, render_template
-from datetime import datetime, timedelta
+from datetime import datetime
 import pytz
 
 app = Flask(__name__)
+
 
 def get_moscow_time():
     moscow_timezone = pytz.timezone('Europe/Moscow')
     moscow_time = datetime.now(moscow_timezone)
     return moscow_time.strftime('%Y-%m-%d %H:%M:%S')
 
+
 @app.route('/')
 def index():
     moscow_time = get_moscow_time()
     return render_template('index.html', moscow_time=moscow_time)
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
--- app_python/test_app.py
+++ app_python/test_app.py
@@ -11,10 +11,11 @@ class TestApp(unittest.TestCase):
         result = get_moscow_time()
 
         moscow_timezone = pytz.timezone('Europe/Moscow')
-        expected_result = datetime.now(moscow_timezone).strftime('%Y-%m-%d %H:%M:%S')
-
+        current_time = datetime.now(moscow_timezone)
+        expected_result = current_time.strftime('%Y-%m-%d %H:%M:%S')
         # Assert that the result matches the expected result
         self.assertEqual(result, expected_result)
+
 
 if __name__ == '__main__':
     unittest.main()
