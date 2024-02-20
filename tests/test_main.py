5f802aeb885761101b3e6e418a53e9a198712ea2
zaqbez39me <korolov_k@mail.ru>
2024-02-20 19:33:38 +0300

    Almost finished with workflows

--- tests/test_main.py
+++ tests/test_main.py
@@ -13,7 +13,7 @@ client = TestClient(app)
 
 
 def string_to_datetime(dt: str) -> datetime:
-    return datetime.strptime(dt, '%Y-%m-%dT%H:%M:%S.%f%z')
+    return datetime.strptime(dt, "%Y-%m-%dT%H:%M:%S.%f%z")
 
 
 def get_utc_offset(dt: datetime) -> timedelta:
@@ -31,12 +31,16 @@ class TestGlobalMoscowTime:
         """
         Testing that the response has valid status code.
         """
-        assert response.status_code == status.HTTP_200_OK, "Wrong response status code!"
+        assert (
+                response.status_code == status.HTTP_200_OK
+        ), "Wrong response status code!"
 
     def test_global_moscow_time_time_format(self, response):
         response_json = response.json()
         try:
-            datetime.strptime(response_json["moscow_time"], '%Y-%m-%dT%H:%M:%S.%f%z')
+            datetime.strptime(
+                response_json["moscow_time"], "%Y-%m-%dT%H:%M:%S.%f%z"
+            )
         except ValueError:
             raise pytest.fail("Wrong datetime format!")
 
@@ -46,15 +50,22 @@ class TestGlobalMoscowTime:
         """
         response_json = response.json()
         api_moscow_datetime = string_to_datetime(response_json["moscow_time"])
-        assert get_utc_offset(api_moscow_datetime) == \
-               get_utc_offset(datetime.now(pytz.timezone("Europe/Moscow"))), "Wrong response timezone!"
+        assert get_utc_offset(api_moscow_datetime) == get_utc_offset(
+            datetime.now(pytz.timezone("Europe/Moscow"))
+        ), "Wrong response timezone!"
 
     def test_global_moscow_time_time(self, response):
         """
-        Testing the correspondence of the api time to the NTP server Moscow time.
+        Testing the correspondence of the api time to the NTP server
+        Moscow time.
         """
         response_json = response.json()
-        api_moscow_time = string_to_datetime(response_json["moscow_time"]).timestamp()
-        current_moscow_time = float(ntplib.NTPClient().request("pool.ntp.org").tx_time)
-        assert api_moscow_time == pytest.approx(current_moscow_time, 1.0e-3), \
-            "Wrong response time!"
+        api_moscow_time = string_to_datetime(
+            response_json["moscow_time"]
+        ).timestamp()
+        current_moscow_time = float(
+            ntplib.NTPClient().request("pool.ntp.org").tx_time
+        )
+        assert api_moscow_time == pytest.approx(
+            current_moscow_time, 1.0e-3
+        ), "Wrong response time!"
