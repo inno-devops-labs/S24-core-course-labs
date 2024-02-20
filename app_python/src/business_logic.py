41203d066e4d2c7cf510a62d4e8d97d9570d61e6
Ilnar <4sitescarp@gmail.com>
2024-02-20 23:36:57 +0300

    Fake file break

--- app_python/src/business_logic.py
+++ app_python/src/business_logic.py
@@ -8,6 +8,7 @@ def get_current_moscow_time() -> datetime:
     return datetime.now(moscow_tz)
 
 
+
 def get_human_readable_time(dt: datetime) -> str:
     human_readable = dt.strftime("%a, %b %d %Y, %H:%M:%S")
     return human_readable
