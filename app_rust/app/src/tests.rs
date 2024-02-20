6c7b94b1c89924daf9dddef1a38ed321995ac1bf
ADari-Ka <ADari.Ka@yandex.ru>
2024-02-20 20:34:05 +0300

    ref(rust): format code to pass future checks

--- app_rust/app/src/tests.rs
+++ app_rust/app/src/tests.rs
@@ -1,18 +1,17 @@
 #[cfg(test)]
 mod tests {
+    use actix_web::{http::header::ContentType, test, web, App};
     use std::sync::Mutex;
-    use actix_web::{http::header::ContentType, web, test, App};
 
     use crate::service::{handlers, state};
 
     #[actix_web::test]
     async fn test_get() {
-        let counter = web::Data::new(
-            state::AppState {
-                counter: Mutex::new(0),
-            }
-        );
-        let app = test::init_service(App::new().app_data(counter.clone()).service(handlers::base)).await;
+        let counter = web::Data::new(state::AppState {
+            counter: Mutex::new(0),
+        });
+        let app =
+            test::init_service(App::new().app_data(counter.clone()).service(handlers::base)).await;
         let req = test::TestRequest::default()
             .insert_header(ContentType::plaintext())
             .to_request();
@@ -24,12 +23,11 @@ mod tests {
 
     #[actix_web::test]
     async fn test_count() {
-        let counter = web::Data::new(
-            state::AppState {
-                counter: Mutex::new(0),
-            }
-        );
-        let app = test::init_service(App::new().app_data(counter.clone()).service(handlers::base)).await;
+        let counter = web::Data::new(state::AppState {
+            counter: Mutex::new(0),
+        });
+        let app =
+            test::init_service(App::new().app_data(counter.clone()).service(handlers::base)).await;
         // first request to get request number == 1
         let req = test::TestRequest::default()
             .insert_header(ContentType::plaintext())
@@ -38,7 +36,7 @@ mod tests {
         let resp = test::call_service(&app, req).await;
 
         assert_eq!(test::read_body(resp).await, "Request number: 1");
-        
+
         // second request to check number updating
         let req = test::TestRequest::default()
             .insert_header(ContentType::plaintext())
