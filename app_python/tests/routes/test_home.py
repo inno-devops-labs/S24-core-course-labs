784422f2cd1a9e7c0b07ad61905d59af8cfbccc9
kamil <almetov.kamil@gmail.com>
2024-02-21 11:10:11 +0300

    Finish lab 3

--- app_python/tests/routes/test_home.py
+++ app_python/tests/routes/test_home.py
@@ -1,24 +1,12 @@
-import datetime
 from unittest import TestCase
-from api.services.moscow_time import get_current_moscow_time_str
 
 from app import create_app
-
 
 
 class TestWelcome(TestCase):
     def setUp(self):
         self.app = create_app().test_client()
 
-    def test_welcome(self):
-        """
-        Tests the route screen message
-        """
-        rv = self.app.get('/')
-
-        cur_time = get_current_moscow_time_str()
-
-        # If we recalculate the hash on the block we should get the same result as we have stored
-        # self.assertEqual({"message": 'Hello World!'}, rv.get_json())
-        assert 1 == 1
-        # assert cur_time in rv.text
+    def test_date_display(self):
+        res = self.app.get("/")
+        assert res.status_code == 200
