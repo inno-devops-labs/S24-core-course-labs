98855d5ccbbb08f2e53d32d834242b52d809dd4f
Grisha Rybolovlev <rybolovlev.grisha@gmail.com>
2024-02-21 18:26:43 +0300

    Tests

--- webapp/tests/test_views.py
+++ webapp/tests/test_views.py
@@ -1,13 +1,11 @@
 from django.test import TestCase, RequestFactory
-
 import os
 import sys
-
 current_directory = os.getcwd()
 parent_directory = os.path.dirname(current_directory)
 sys.path.append(parent_directory)
-
 from msk_time import views
+
 
 class MyViewTestCase(TestCase):
     def setUp(self):
