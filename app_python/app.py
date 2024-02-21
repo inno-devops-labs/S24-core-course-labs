25d7b0734808a7d145b825683d5e4141589d4498
Azamat <alkuat1511@gmail.com>
2024-02-21 04:09:53 +0300

    18 try

--- app_python/app.py
+++ app_python/app.py
@@ -1,20 +1,20 @@
 from flask import Flask, render_template
 from datetime import datetime
 import pytz
-#import os
-import subprocess
 
 # Create a non-root user if it doesn't exist
-#subprocess.call(['adduser', '-D', 'myuser'])
-#os.setuid(1000)  # Switch to non-root user
+# subprocess.call(['adduser', '-D', 'myuser'])
+# os.setuid(1000)  # Switch to non-root user
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     moscow_tz = pytz.timezone('Europe/Moscow')
     moscow_time = datetime.now(moscow_tz).strftime('%Y-%m-%d %H:%M:%S')
     return render_template('index.html', time=moscow_time)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
