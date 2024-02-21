644a90d17d4cc52c754d4823ce76cc467a66542a
Inga Ezhova <i.ezhova@innopolis.university>
2024-02-21 10:34:04 +0300

    lint improvements

--- app_python/app.py
+++ app_python/app.py
@@ -1,16 +1,26 @@
-from flask import Flask, render_template
+"""
+This module defines a Flask application to display the current time in Moscow.
+"""
+
 from datetime import datetime
 import pytz
+from flask import Flask
 
 app = Flask(__name__)
+
 
 # Define a route to display the current time in Moscow
 @app.route('/')
 def display_time():
+    """
+    Display the current time in Moscow.
+    """
     city_timezone = pytz.timezone('Europe/Moscow')
     city_time = datetime.now(city_timezone)
     formatted_city_time = city_time.strftime('%Y-%m-%d %H:%M:%S')
     return f'Current time in the city: {formatted_city_time}'
 
+
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
+
