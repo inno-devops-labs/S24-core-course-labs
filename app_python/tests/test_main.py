e10bca47838568bd876a49fca3dbc88b72158e89
ahmedXDR <a.soliman@innopolis.university>
2024-02-21 09:35:48 +0300

    update python test

--- app_python/tests/test_main.py
+++ app_python/tests/test_main.py
@@ -1,4 +1,5 @@
 import time
+import re
 
 from fastapi.testclient import TestClient
 
@@ -13,14 +14,38 @@ def test_time_updates():
     """
     # Make the first request
     response1 = client.get("/")
-    time1 = response1.text
+
+    # Extract time from html response
+    time1 = response1.text.split("<p>")[1].split("</p>")[0]
 
     # Wait for a second to ensure the time changes
     time.sleep(5)  # Wait for at least one second
 
     # Make the second request
     response2 = client.get("/")
-    time2 = response2.text
+
+    # Extract time from html response
+    time2 = response2.text.split("<p>")[1].split("</p>")[0]
 
     # Check if the responses are different
     assert time1 != time2, "The time did not update between requests."
+
+
+def test_time_format():
+    """
+    Test that the time is in the correct format.
+    """
+    # Make the request
+    response = client.get("/")
+
+    # Extract time from html response
+    time = response.text.split("<p>")[1].split("</p>")[0]
+
+    # log time in test
+    print(time)
+
+    # Check if the time is in the correct format (should match "%H:%M:%S")
+    assert re.match(
+        r"^\d{2}:\d{2}:\d{2}$",
+        time,
+    ), "The time is not in the correct format."
