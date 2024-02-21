758b1490f6250e387faf44f0e055dd91e2f0f04c
Azamat <alkuat1511@gmail.com>
2024-02-21 04:01:31 +0300

    16 try

--- app_python/test_app.py
+++ app_python/test_app.py
@@ -7,7 +7,10 @@ def client():
     with app.test_client() as client:
         yield client
 
+@pytest.mark.filterwarnings("ignore:.*url_parse.*:DeprecationWarning")
+@pytest.mark.filterwarnings("ignore:.*URL.*:DeprecationWarning")
 def test_index(client):
     response = client.get('/')
-    assert response.status_code ==  200
+    assert response.status_code ==   200
     assert b'Current Time in Moscow' in response.data
+
