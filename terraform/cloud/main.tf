71d17835abacfd18e3f47bca9d65d0475220ce61
FurryLord <rusin.senya03@gmail.com>
2024-02-25 13:39:49 +0300

    move from Windows VM to linux VM

--- terraform/cloud/main.tf
+++ terraform/cloud/main.tf
@@ -4,84 +4,104 @@ terraform {
       source = "yandex-cloud/yandex"
     }
   }
+  required_version = ">= 0.13"
 }
 
 provider "yandex" {
-  zone = var.zone
-  token = "t1.9euelZqcnpbPx56ak5CQm52Rx8yYk-3rnpWamJWTlszJl4vHnczIipCQiovl9PdiQzxR-e9odDTV3fT3InI5UfnvaHQ01c3n9euelZrNiceKkMbKy5rLioqPxpuaj-_8zef1656VmseNy4zPlJvLiZWenZTJzo2X7_3F656Vms2Jx4qQxsrLmsuKio_Gm5qP.Lsn2JP3SF47YEjPcIrNQyUDHeUieTlSgoXKl7DhzGjD8SZZGx0Y9Bl4lETsydHR6xXnwMW_rfWSnbhFSedbWDw"
+  zone  = "ru-central1-a"
+  token = "t1.9euelZqci47Kl5qQi4zMzJHHi4-Rie3rnpWamJWTlszJl4vHnczIipCQiovl8_dFahNR-e8mUgpV_d3z9wUZEVH57yZSClX9zef1656VmsaMjYnMlouTzc6bypOPzMea7_zN5_XrnpWazcuVmZiVksaZk4-Pi4uZyZvv_cXrnpWaxoyNicyWi5PNzpvKk4_Mx5o.-Xly9g-SDYvNNSS7GSu_DmKy15GIV6jEBxmXHuHWOogk5NNwc3Iez2lpa15NqF2dq7j5kQqfCzi3LfJ_DZuwBg"
 }
 
-resource "yandex_vpc_network" "default" {
-  name = var.network
+resource "yandex_compute_disk" "boot-disk-1" {
+  name      = "boot-disk-1"
+  type      = "network-hdd"
+  zone      = "ru-central1-a"
+  size      = "20"
+  image_id  = "fd87ap2ld09bjiotu5v0"
   folder_id = "b1gvup3s47j516kd9hdh"
 }
 
-resource "yandex_vpc_subnet" "default" {
-  network_id     = yandex_vpc_network.default.id
-  name           = var.subnet
-  v4_cidr_blocks = ["192.168.10.0/24"]
-  zone           = var.zone
+resource "yandex_compute_disk" "boot-disk-2" {
+  name      = "boot-disk-2"
+  type      = "network-hdd"
+  zone      = "ru-central1-a"
+  size      = "20"
+  image_id  = "fd87ap2ld09bjiotu5v0"
   folder_id = "b1gvup3s47j516kd9hdh"
 }
 
-resource "yandex_compute_image" "default" {
-  source_family  = var.image_family
-  folder_id = "b1gvup3s47j516kd9hdh"
-}
-
-
-
-resource "yandex_compute_disk" "boot-disk" {
-  name     = "boot-disk"
-  type     = var.disk_type
-  zone     = "ru-central1-a"
-  size     = var.disk_size
-  image_id = yandex_compute_image.default.id
-  folder_id = "b1gvup3s47j516kd9hdh"
-}
-
-data "template_file" "default" {
-  template = file("${path.module}/init.ps1")
-  vars = {
-    user_name  = var.user_name
-    user_pass  = var.user_pass
-    admin_pass = var.admin_pass
-  }
-}
-
-resource "yandex_compute_instance" "default" {
-  zone     = var.zone
+resource "yandex_compute_instance" "vm-1" {
+  name = "terraform1"
 
   resources {
-    cores  = var.cores
-    memory = var.memory
+    cores  = 2
+    memory = 2
   }
 
   boot_disk {
-    disk_id = yandex_compute_disk.boot-disk.id
+    disk_id = yandex_compute_disk.boot-disk-1.id
   }
 
   network_interface {
-    subnet_id = yandex_vpc_subnet.default.id
-    nat       = var.nat
+    subnet_id = yandex_vpc_subnet.subnet-1.id
+    nat       = true
   }
 
   metadata = {
-    user-data = data.template_file.default.rendered
-  }
-
-  timeouts {
-    create = var.timeout_create
-    delete = var.timeout_delete
+    ssh-keys = "ubuntu:${file("~/.ssh/id_ed25519.pub")}"
   }
   folder_id = "b1gvup3s47j516kd9hdh"
 }
 
-output "name" {
-  value = yandex_compute_instance.default.name
+resource "yandex_compute_instance" "vm-2" {
+  name = "terraform2"
+
+  resources {
+    cores  = 4
+    memory = 4
+  }
+
+  boot_disk {
+    disk_id = yandex_compute_disk.boot-disk-2.id
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet-1.id
+    nat       = true
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file("~/.ssh/id_ed25519.pub")}"
+  }
+  folder_id = "b1gvup3s47j516kd9hdh"
 }
 
-output "address" {
-  value = yandex_compute_instance.default.network_interface.0.nat_ip_address
+resource "yandex_vpc_network" "network-1" {
+  name      = "network1"
+  folder_id = "b1gvup3s47j516kd9hdh"
+}
+
+resource "yandex_vpc_subnet" "subnet-1" {
+  name           = "subnet1"
+  zone           = "ru-central1-a"
+  network_id     = yandex_vpc_network.network-1.id
+  v4_cidr_blocks = ["192.168.10.0/24"]
+  folder_id = "b1gvup3s47j516kd9hdh"
+}
+
+output "internal_ip_address_vm_1" {
+  value = yandex_compute_instance.vm-1.network_interface.0.ip_address
+}
+
+output "internal_ip_address_vm_2" {
+  value = yandex_compute_instance.vm-2.network_interface.0.ip_address
+}
+
+output "external_ip_address_vm_1" {
+  value = yandex_compute_instance.vm-1.network_interface.0.nat_ip_address
+}
+
+output "external_ip_address_vm_2" {
+  value = yandex_compute_instance.vm-2.network_interface.0.nat_ip_address
 }
 
