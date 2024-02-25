64375a3949eda5f5b5133754153000cc6aa6cb28
Roman Molochkov <molochkov2017@gmail.com>
2024-02-25 22:53:21 +0300

    add best practices

--- app_python/terraform/github/main.tf
+++ app_python/terraform/github/main.tf
@@ -1,5 +1,5 @@
-resource "github_repository" "Devops-core-course-labs" {
-  name               = "Devops-core-course-labs"
+resource "github_repository" "S24-DevOps-labs" {
+  name               = "S24-DevOps-labs"
   description        = "This is a repository for the DevOps Core Course Labs"
   visibility         = "public"
   has_issues         = true
@@ -9,13 +9,14 @@ resource "github_repository" "Devops-core-course-labs" {
   gitignore_template = "Python"
 }
 
+
 resource "github_branch_default" "main" {
-  repository = github_repository.Devops-core-course-labs.name
+  repository = github_repository.S24-DevOps-labs.name
   branch     = "main"
 }
 
 resource "github_branch_protection" "default" {
-  repository_id                   = github_repository.Devops-core-course-labs.id
+  repository_id                   = github_repository.S24-DevOps-labs.id
   pattern                         = github_branch_default.main.branch
   require_conversation_resolution = true
   enforce_admins                  = true
