eaef02aea0d60eb64d8032973dc21cd13fb8606b
Zeyad Alagamy <wezza2003@gmail.com>
2024-02-27 23:47:19 +0300

    add github and tutorials tasks fix gitignore

---
+++ terraform/tutorials/learn-terraform-aws-instance/main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+variable "name" {
+  description = "The name of the EC2 instance"
+  type        = string
+  default     = "example-instance"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = var.name
+  }
+}
