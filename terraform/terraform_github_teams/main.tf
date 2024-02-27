ff98e937b614fc80158f83ac0eccf67d202840b0
IlnurHA <53339563+IlnurHA@users.noreply.github.com>
2024-02-28 01:03:09 +0300

    Teams

---
+++ terraform/terraform_github_teams/main.tf
@@ -0,0 +1,41 @@
+provider "github" {
+  token = file("C:/DevOps/gh_token.txt")
+  owner = "DevOpsCourse-S24-Terraform"
+}
+# Organization url: https://github.com/DevOpsCourse-S24-Terraform
+
+resource "github_repository" "example-repo" {
+  name        = "example-repo"
+  description = "My new repository for use with Terraform"
+}
+# example-repo: https://github.com/DevOpsCourse-S24-Terraform/example-repo
+
+resource "github_team" "example-team" {
+  name        = "example-team"
+  description = "My new team for use with Terraform"
+  privacy     = "closed"
+}
+# example-team: https://github.com/orgs/DevOpsCourse-S24-Terraform/teams/example-team
+
+resource "github_team" "admin-team" {
+  name        = "admin-team"
+  description = "My new team for use with Terraform"
+  privacy     = "closed"
+}
+# Admin team: https://github.com/orgs/DevOpsCourse-S24-Terraform/teams/admin-team
+
+resource "github_team_repository" "example-team-repo" {
+  team_id    = github_team.example-team.id
+  repository = github_repository.example-repo.name
+  permission = "push"
+}
+# Example team repositories: https://github.com/orgs/DevOpsCourse-S24-Terraform/teams/example-team/repositories
+
+resource "github_team_repository" "example-admin-team-repo" {
+  team_id    = github_team.admin-team.id
+  repository = github_repository.example-repo.name
+  permission = "admin"
+}
+# Admin team repositories: https://github.com/orgs/DevOpsCourse-S24-Terraform/teams/admin-team/repositories
+
+# Tutorial: https://www.hashicorp.com/blog/managing-github-with-terraform
