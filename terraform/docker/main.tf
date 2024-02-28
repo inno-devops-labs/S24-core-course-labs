ba2d5230a3c500df926944f697455c8165bbc2c5
Demezy <38487319+Demezy@users.noreply.github.com>
2024-02-28 10:47:21 +0300

    feat(lab4): implement lab4

---
+++ terraform/docker/main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0.1"
+    }
+  }
+}
+
+variable "container_name" {
+  type    = string
+  default = "container-name"
+}
+
+provider "docker" {}
+
+resource "docker_image" "nginx" {
+  name         = "nginx:latest"
+  keep_locally = false
+}
+
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.image_id
+  name  = var.container_name
+  ports {
+    internal = 80
+    external = 8000
+  }
+}
