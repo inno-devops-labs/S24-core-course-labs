e8907aec2ffb76bacf853a2605c09be632e8cb2c
fatm1nd <semeritskiy@yandex.ru>
2024-02-29 10:23:08 +0300

    Lab4

---
+++ terraform/yandex-cloud/variables.tf
@@ -0,0 +1,16 @@
+variable "yandex-cloud-id" {
+  description = "Id for cloud"
+  type        = string
+  default     = "<id>"
+}
+
+variable "yandex-folder-id" {
+  description = "Id for folder"
+  type        = string
+  default     = "<id>"
+}
+
+variable "secret-token" {
+  description = "Secret token for authorization"
+  type        = string
+}
