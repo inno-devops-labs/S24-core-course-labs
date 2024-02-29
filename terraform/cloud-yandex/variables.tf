69235fb5439e3cfba9c1ca8011a43e25f6e9663a
exemplerie <exemplerie@mail.ru>
2024-02-29 10:09:56 +0300

    confidential data delete

--- terraform/cloud-yandex/variables.tf
+++ terraform/cloud-yandex/variables.tf
@@ -1,17 +1,14 @@
 variable "cloud-id" {
   description = "Cloud Id"
   type        = string
-  default     = "b1g0bqhf0c67vkjaap04"
 }
 
 variable "folder-id" {
   description = "Folder Id"
   type        = string
-  default     = "b1gg65k13122o9pouqi4"
 }
 
 variable "token" {
   description = "Authorization token"
   type        = string
-  default     = "y0_AgAAAAAg7naCAATuwQAAAAD8OfIfAAC511sScGtNZqfkAcuy1LJaJshCBw"
 }
