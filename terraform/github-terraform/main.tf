722f9cb38779bc09781151be53ad25aad101b99f
hugowea <z.azizov@innopolis.university>
2024-03-03 23:28:50 +0300

    added solution for lab4

---
+++ terraform/github-terraform/main.tf
@@ -0,0 +1,42 @@
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "github" {
+  token = var.token # or `GITHUB_TOKEN`
+}
+
+#Create and initialise a public GitHub Repository with MIT license and a Visual Studio .gitignore file (incl. issues and wiki)
+resource "github_repository" "S24-core-course-labs" {
+  name               = "S24-core-course-labs"
+  description        = "awesome lab"
+  visibility         = "public"
+  has_issues         = true
+  has_wiki           = true
+  auto_init          = true
+  license_template   = "mit"
+  gitignore_template = "VisualStudio"
+}
+
+#Set default branch 'master'
+resource "github_branch_default" "main" {
+  repository = github_repository.S24-core-course-labs.name
+  branch     = "main"
+}
+
+#Create branch protection rule to protect the default branch. (Use "github_branch_protection_v3" resource for Organisation rules)
+resource "github_branch_protection" "default" {
+  repository_id                   = github_repository.S24-core-course-labs.id
+  pattern                         = github_branch_default.main.branch
+  require_conversation_resolution = true
+  enforce_admins                  = true
+
+  required_pull_request_reviews {
+    required_approving_review_count = 2
+  }
+}
