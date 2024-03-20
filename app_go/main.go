9a39179cf2c4fd9749725edf6ba9f81a457bf399
y4cer <a.buchnev@innopolis.university>
2024-03-20 10:08:34 +0300

    add logging stack files

--- app_go/main.go
+++ app_go/main.go
@@ -1,13 +1,25 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 )
 
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("Got request with url: %v\n", r.URL)
+}
+
 func main() {
+
 	fs := http.FileServer(http.Dir("static"))
 
 	http.Handle("/", fs)
+	http.HandleFunc("/api", handler)
 
-	http.ListenAndServe(":8080", nil)
+	fmt.Println("Starting server")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		fmt.Println("Failed to start the server")
+
+	}
+
 }
