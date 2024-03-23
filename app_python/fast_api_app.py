1cc7a8067ff0bf2f0a9aa5d6d378a79f613b9a0f
IlnurHA <Ilnur_ha@mail.ru>
2024-03-23 16:16:09 +0300

    Prometheus + FastAPI

--- app_python/fast_api_app.py
+++ app_python/fast_api_app.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 
 from api import router as api_router
+from prometheus_fastapi_instrumentator import Instrumentator
 
 app = FastAPI()
 app.include_router(api_router, prefix="/api")
+
+Instrumentator().instrument(app).expose(app)
