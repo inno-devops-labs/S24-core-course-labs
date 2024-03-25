d8e58702bd9e8f21ae5c644425f604d8a5d1806a
ilnarkhasanov <4sitescarp@gmail.com>
2024-03-25 20:58:54 +0300

    Add metrics to the Python app

--- app_python/src/app.py
+++ app_python/src/app.py
@@ -1,10 +1,14 @@
 from datetime import datetime
 
 from fastapi import FastAPI
+from prometheus_client import make_asgi_app
 
 from src.business_logic import get_current_moscow_time, get_human_readable_time
 
 app = FastAPI(title="Moscow Time")
+
+metrics_app = make_asgi_app()
+app.mount("/metrics", metrics_app)
 
 
 @app.get("/")
