4004c392f726572f7a9b52ebefb417651568b034
pptx704 <rafeedm.bhuiyan@gmail.com>
2024-03-27 01:25:05 +0300

    Updated python for metrics

--- app_python/app.py
+++ app_python/app.py
@@ -3,10 +3,12 @@ Main application file. Contains the route information and necessary setups.
 """
 
 from flask import Flask
+from prometheus_flask_exporter import PrometheusMetrics
+
 from app_python.app_utils import return_time
 
 app = Flask(__name__)
-
+metrics = PrometheusMetrics(app)
 
 @app.route("/")
 def show_time():
@@ -14,3 +16,10 @@ def show_time():
     Index page. Shows the current time in Moscow.
     """
     return return_time("Europe/Moscow")
+
+@app.route("/metrics")
+def show_prometheus_metrics():
+    """
+    Metrics page. Shows the current time in Moscow in Prometheus format.
+    """
+    return return_time("Europe/Moscow", prometheus=True)
