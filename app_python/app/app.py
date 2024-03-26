eb38e4443e7e07bce25c3ae637249b971e2a84ae
Nikita Fedorovich <n.fedorovich@innopolis.university>
2024-03-27 01:40:26 +0300

    .

--- app_python/app/app.py
+++ app_python/app/app.py
@@ -3,11 +3,17 @@ from datetime import datetime
 import pytz
 import os
 from dotenv import load_dotenv
+from prometheus_client import Counter, generate_latest, CONTENT_TYPE_LATEST
 
 app = Flask(__name__)
 load_dotenv()
 PORT = os.getenv('PORT')
 HOST = os.getenv('HOST')
+
+grafana_health_check_counter = Counter(
+    'grafana_health_check_total',
+    'Total number of health checks for Grafana',
+)
 
 
 @app.route('/')
@@ -16,6 +22,12 @@ def get_time():
     current_time = datetime.now(moscow_tz).strftime('%Y-%m-%d %H:%M:%S')
     return f'The current time in Moscow is: {current_time}'
 
+@app.route('/metrics')
+def metrics():
+    grafana_health_check_counter.inc()
+    prometheus_metrics = generate_latest()
+
+    return prometheus_metrics, 200, {'Content-Type': CONTENT_TYPE_LATEST}
 
 if __name__ == '__main__':
     app.run(host=HOST, port=PORT, debug=True)
