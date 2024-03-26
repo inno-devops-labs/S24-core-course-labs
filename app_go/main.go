45e45833257dd8617ba4baec9ec53a25d9618380
y4cer <a.buchnev@innopolis.university>
2024-03-26 22:43:55 +0300

    lab8

--- app_go/main.go
+++ app_go/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -15,11 +17,12 @@ func main() {
 
 	http.Handle("/", fs)
 	http.HandleFunc("/api", handler)
+	http.Handle("/metrics", promhttp.Handler())
 
 	fmt.Println("Starting server")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	if err := http.ListenAndServe(":8082", nil); err != nil {
 		fmt.Println("Failed to start the server")
-
+		fmt.Println("%v", err)
 	}
 
 }
