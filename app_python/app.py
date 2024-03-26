c60a96288d2e2e90e933107a08337d0ab7521ec2
metafates <vvz.dev@gmail.com>
2024-03-26 23:14:24 +0300

    lab8

--- app_python/app.py
+++ app_python/app.py
@@ -4,15 +4,24 @@ import pathlib
 import pytz
 from fastapi import FastAPI, HTTPException
 from starlette.staticfiles import StaticFiles
+from prometheus_fastapi_instrumentator import Instrumentator
 
 dir_path = pathlib.Path(__file__).parent.resolve()
 
 app = FastAPI(title="Main app")
+
+instrumentator = Instrumentator().instrument(app)
+
 api = FastAPI(title="Api app")
 
 
 def get_time_with_tz(tz: str):
     return datetime.now(pytz.timezone(tz)).strftime("%Y-%m-%d %H:%M:%S")
+
+
+@app.on_event("startup")
+async def _startup():
+    instrumentator.expose(app)
 
 
 @api.get("/time")
---
+++ monitoring/METRICS.md
@@ -0,0 +1,14 @@
+# Metrics
+
+## Log rotation
+
+```yaml
+max-size: 20M
+max-file: 10
+```
+
+## Screenshots
+
+![Image 1](./screenshots/grafana.jpg)
+
+![Image 2](./screenshots/prometheus.jpg)
