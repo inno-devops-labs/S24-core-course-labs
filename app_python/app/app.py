584a33e2a1d98100d060d3cf4f4945deb9b29c14
dmfrpro <dmfr2021y@gmail.com>
2024-03-27 00:42:49 +0300

    Lab 8 - Dmitrii Alekhin
    Signed-off-by: dmfrpro <dmfr2021y@gmail.com>

--- app_python/app/app.py
+++ app_python/app/app.py
@@ -4,6 +4,7 @@ Author: Dmitrii Alekhin (d.alekhin@innopolis.university)
 """
 
 from datetime import datetime, timezone, timedelta
+from prometheus_flask_exporter import PrometheusMetrics
 from os import environ
 from flask import Flask, render_template
 
@@ -12,26 +13,20 @@ HOST = environ.get("HOST", "0.0.0.0")
 PORT = int(environ.get("PORT", "8080"))
 
 
-def create_app() -> Flask:
+wsgi_app = Flask(__name__)
+metrics = PrometheusMetrics(wsgi_app)
+metrics.info("app_info", "Application info", version="1.0.0")
+
+
+@wsgi_app.route("/", methods=["GET"])
+def index():
     """
-    Creates an application and configures routes
+    Returns an html page with current time in Moscow
     """
-    app = Flask(__name__)
-
-    @app.route("/", methods=["GET"])
-    def index():
-        """
-        Returns an html page with current time in Moscow
-        """
-        zone = timezone(timedelta(hours=3))
-        time = datetime.now(timezone.utc).astimezone(zone)
-        return render_template("index.html", time=time)
-
-    return app
-
-
-wsgi_app = create_app()
+    zone = timezone(timedelta(hours=3))
+    time = datetime.now(timezone.utc).astimezone(zone)
+    return render_template("index.html", time=time)
 
 
 if __name__ == "__main__":
-    wsgi_app.run(host=HOST, port=PORT, debug=False)
+    wsgi_app.run(host=HOST, port=PORT)
