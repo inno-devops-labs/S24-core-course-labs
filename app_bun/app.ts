d36522cd7e0a48f800f763cc4845a26e6e0fbd4b
pptx704 <rafeedm.bhuiyan@gmail.com>
2024-03-27 05:01:51 +0300

    Updated bun app with prom-client

--- app_bun/app.ts
+++ app_bun/app.ts
@@ -1,9 +1,37 @@
 // Purpose: This is the main logic section for the app. It handles the request and response logic.
 
 import { returnTime } from "./utils.js";
+import { Registry, Counter } from "prom-client";
+
+// Create a Registry
+const registry = new Registry();
+
+registry.setDefaultLabels({
+  app: "app_bun",
+});
+
+// export function index() {
+//   return new Response(returnTime(), {
+//     headers: { "content-type": "text/html" },
+//   });
+// }
+
+// Collect default metrics without using the default function
+const counter = new Counter({
+  name: "app_bun_counter",
+  help: "Example of a counter",
+  registers: [registry],
+});
 
 export function index() {
+  counter.inc();
   return new Response(returnTime(), {
     headers: { "content-type": "text/html" },
+  });
+}
+
+export async function metrics() {
+  return new Response(await registry.metrics(), {
+    headers: { "content-type": "text/plain" },
   });
 }
--- app_bun/entry.ts
+++ app_bun/entry.ts
@@ -1,6 +1,15 @@
 // Purpose: Entry point for the Bun server.
 
-import { index } from "./app.ts";
+import { index, metrics } from "./app.ts";
+// use prom-client to expose metrics
 
-const server = Bun.serve({ fetch: index });
+const server = Bun.serve({
+    fetch(req) {
+        const url = new URL(req.url);
+        if (url.pathname === "/metrics") {
+            return metrics();
+        }
+        return index();
+    }
+})
 console.log(`Running http://localhost:${server.port}`);
