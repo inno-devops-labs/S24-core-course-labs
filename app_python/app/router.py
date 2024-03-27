ebe167edf585999923dc1f925d5eabc6e4ec4fcb
Alexandr Ragulin <a.ragulin@tinkoff.ru>
2024-03-27 09:35:56 +0300

    finish lab8

--- app_python/app/router.py
+++ app_python/app/router.py
@@ -1,8 +1,18 @@
 import logging
 
-from .datetime_formatter import DateTimeFormatted
-from flask import Blueprint, render_template
-from .moscow_date_time import get_moscow_date_time
+import prometheus_client
+from datetime_formatter import DateTimeFormatted
+from flask import Blueprint, Response, render_template
+from moscow_date_time import get_moscow_date_time
+from prometheus_client import Counter
+
+APP_NAME = 'app_python'
+CONTENT_TYPE_LATEST = str('text/plain; version=0.0.4; charset=utf-8')
+REQUEST_COUNT = Counter(
+    'request_count',
+    'App Request Count',
+    ['app_name', 'method', 'endpoint', 'http_status'],
+)
 
 router = Blueprint('router', __name__)
 
@@ -12,6 +22,9 @@ router = Blueprint('router', __name__)
 def display_date_time() -> str:
     logger = logging.getLogger(__name__)
     logger.debug('Handling request')
+    REQUEST_COUNT.labels(
+        app_name=APP_NAME, method='GET', endpoint='/', http_status='200'
+    ).inc()
     try:
         now = get_moscow_date_time()
         date_time: DateTimeFormatted = DateTimeFormatted.from_datetime(now)
@@ -27,3 +40,13 @@ def display_date_time() -> str:
         return render_template(
             'index.html', current_date='N/A', current_time='N/A'
         )
+
+
+@router.route('/metrics')
+def metrics() -> str:
+    REQUEST_COUNT.labels(
+        app_name=APP_NAME, method='GET', endpoint='/metrics', http_status='200'
+    ).inc()
+    return Response(
+        prometheus_client.generate_latest(), mimetype=CONTENT_TYPE_LATEST
+    )
