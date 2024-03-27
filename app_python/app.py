34f2cda87e3cd404a7b363a198c58b0fa0c8c364
Inga Ezhova <i.ezhova@innopolis.university>
2024-03-27 10:10:56 +0300

    Lab8

--- app_python/app.py
+++ app_python/app.py
@@ -4,8 +4,8 @@ This module defines a Flask application to display the current time in Moscow.
 
 from datetime import datetime
 import pytz
-from flask import Flask
-
+from flask import Flask, Response
+from prometheus_client import Counter, Gauge, generate_latest
 app = Flask(__name__)
 
 
@@ -19,6 +19,14 @@ def display_time():
     city_time = datetime.now(city_timezone)
     formatted_city_time = city_time.strftime('%Y-%m-%d %H:%M:%S')
     return f'Current time in the city: {formatted_city_time}'
+
+
+@app.route("/metrics")
+def metrics():
+    """
+    Returns Prometheus metrics
+    """
+    return Response(generate_latest(), mimetype='text/plain')
 
 
 if __name__ == '__main__':
