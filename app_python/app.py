27d60e5097471854b13a3d766c145101a6b864f0
hugowea <z.azizov@innopolis.university>
2024-03-27 05:50:57 +0300

    lab 8 all

--- app_python/app.py
+++ app_python/app.py
@@ -1,18 +1,30 @@
-"""
-This module provides a simple Flask application that displays the current time in Moscow.
-"""
-
+from flask import Flask, render_template, Response
 import datetime
 import pytz
-from flask import Flask, render_template
+from prometheus_client import Counter, generate_latest
 
 app = Flask(__name__)
 
+healthcheck_counter = Counter(
+    'healthcheck_requests',
+    'Number of healthcheck requests'
+)
+
+
+# @app.route('/healthcheck')
+# def healthcheck():
+#     healthcheck_counter.inc()
+#     return 'Ok'
+
+
+@app.route('/metrics')
+def metrics():
+    return Response(generate_latest(), content_type='text/plain')
+
+# Route for the home page
 @app.route('/')
 def home():
-    """
-    Route handler for the home page.
-    """
+
     # Get the current time in Moscow
     moscow_timezone = pytz.timezone('Europe/Moscow')
     current_time = datetime.datetime.now(moscow_timezone).strftime('%Y-%m-%d %H:%M:%S')
