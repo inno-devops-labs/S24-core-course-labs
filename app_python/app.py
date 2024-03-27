865c3c660fae1464702479f8bb9fd525ad66f9d1
nytakoe1 <yana.pavlova.pole@gmail.com>
2024-03-27 10:11:15 +0300

    homework done final

--- app_python/app.py
+++ app_python/app.py
@@ -1,8 +1,12 @@
 # app.py
 
-from flask import Flask, render_template
+from flask import Flask, render_template, Response
 from datetime import datetime
 import pytz
+from prometheus_client import start_http_server, Summary
+import prometheus_client
+
+REQUEST_TIME = Summary('request_processing_seconds', 'Time spent processing request')
 
 app = Flask("Moscow_current_time")
 
@@ -11,6 +15,7 @@ app.static_folder = 'static'
 
 
 @app.route('/')
+@REQUEST_TIME.time()
 def index():
     try:
         # Get the current time in Moscow
@@ -33,6 +38,11 @@ def index():
         # Return a generic error message to the user
         return "An unexpected error occurred. " \
                "Please try again later.", 500
+
+
+@app.route('/metrics')
+def metrics():
+    return Response(prometheus_client.generate_latest(), mimetype="text/plain")
 
 
 if __name__ == '__main__':
---
+++ monitoring/METRICS.md
@@ -0,0 +1,11 @@
+#Metrics
+
+## Targets
+![Targets Dashboard](./photos/photo5.png)
+
+## Prometheus
+![Prometheus Dashboard](./photos/photo7.png)
+
+## Loki
+![Loki Dashboard](./photos/photo6.jpeg)
+
