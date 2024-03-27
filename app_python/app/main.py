50ebfd0724acba822b813c5f9bbe4b2d12cb972b
Ahmad Alhussin <a.alhussin@innopolis.university>
2024-03-27 04:19:50 +0300

    added lab 8

--- app_python/app/main.py
+++ app_python/app/main.py
@@ -1,11 +1,15 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, Response
 from datetime import datetime
 from pytz import timezone
+from prometheus_client import Summary, generate_latest
 
 bp = Blueprint("main", __name__, url_prefix="")
 
+REQUEST_TIME = Summary("request_processing_time", "Time to handle request")
+
 
 @bp.route("/", methods=["GET"])
+@REQUEST_TIME.time()
 def get_time():
     """route to return the time in moscow timezone
 
@@ -15,3 +19,8 @@ def get_time():
     MSK = timezone("Europe/Moscow")
     moscow_time = datetime.now(MSK).strftime("%Y:%m:%d %H:%M:%S %Z %z")
     return jsonify({"time": moscow_time})
+
+
+@bp.route("/metrics")
+def metrics():
+    return Response(generate_latest(), mimetype="text/plain")
