e2593a4c37f890dca4c1d0c31a923db69f32838c
anastasia-martynova <martinovi17@mail.ru>
2024-03-27 07:51:32 +0300

    Lab8

--- app_python/app.py
+++ app_python/app.py
@@ -1,14 +1,20 @@
-from flask import Flask
+from flask import Flask, Response
 from datetime import datetime
+from prometheus_client import generate_latest
 import pytz
 
 app = Flask(__name__)
 
 
+@app.route('/metrics')
+def metrics():
+    return Response(generate_latest(), content_type='text/plain')
+
 @app.route('/')
 def display_time_msk():
     timezone = pytz.timezone('Europe/Moscow')
     time_msk = datetime.now(timezone)
+    print("Received request")
     return f"Current time in Moscow: " \
            f"{time_msk.strftime('%Y-%m-%d %H:%M:%S %Z')}"
 
