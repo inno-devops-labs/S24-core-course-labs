c5b530ebf240ca00b3426f7eac48f694f78a914a
kamil <almetov.kamil@gmail.com>
2024-03-27 11:18:59 +0300

    Finish

--- app_python/app.py
+++ app_python/app.py
@@ -1,16 +1,21 @@
-from flask import Flask
-from api.routes.home import home
+from flask import Flask, render_template
+# from api.routes.home import home
 from prometheus_flask_exporter import PrometheusMetrics
 
+from api.services.moscow_time import get_current_moscow_time_str
 
-def create_app():
-    app = Flask(__name__)
-    app.register_blueprint(home)
-    return app
 
-app = create_app()
+
+app = Flask(__name__)
+# app.register_blueprint(home)
+
 metrics = PrometheusMetrics(app)
 metrics.info('app_info', 'Application info', version='2.0.0')
+
+@app.route('/')
+def current_moscow_time():
+    cur_time_str = get_current_moscow_time_str()
+    return render_template("current_moscow_time.html", time=cur_time_str)
 
 if __name__ == "__main__":
     from argparse import ArgumentParser
@@ -24,4 +29,4 @@ if __name__ == "__main__":
 
     # app = create_app()
     
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port, debug=False)
