12d633fab177aa17b9dbf26a2b2628aa6c6884df
evsey9 <evseyru1@mail.ru>
2024-04-20 06:15:04 +0300

    lab 12

--- app_python/README.md
+++ app_python/README.md
@@ -9,10 +9,13 @@ This simple Python web application built with Flask displays the current time in
 1. Install dependencies using `pip install -r requirements.txt`.
 2. Run the application using `python main.py`.
 3. Open your web browser and go to `http://127.0.0.1:5000/` to view the Moscow time.
+4. Get the visits count from `http://127.0.0.1:5000/visits`.
 
 ## Project Structure
 
 - `app_python/`: Main application folder.
+  - `data/`: Data folder.
+    - `visits.txt`: Visits count.
   - `main.py`: Flask application.
   - `templates/`: HTML templates.
   - `requirements.txt`: Python app requirements.
--- app_python/app/main.py
+++ app_python/app/main.py
@@ -1,12 +1,16 @@
+import os
+
 from flask import Flask, render_template
 from datetime import datetime
 import pytz
+import shelve
 
 app = Flask(__name__)
 
 TIME_FORMAT = "%Y-%m-%d %H:%M:%S"
 TIMEZONE = "Europe/Moscow"
 HOSTNAME = "0.0.0.0"
+VISITS_FILE_PATH = "data/visits.txt"
 
 
 def get_time(timezone):
@@ -17,8 +21,38 @@ def format_time(time, time_format):
     return time.strftime(time_format)
 
 
+@app.get("/visits")
+def get_visits():
+    """
+    Return the number of visits.
+    """
+    with open(VISITS_FILE_PATH, "r") as file:
+        visits = int(file.read())
+    return {"visits": visits}
+
+
+def increment_visits():
+    with open(VISITS_FILE_PATH, "r+") as file:
+        visits = int(file.read())
+        visits += 1
+        file.seek(0)
+        file.write(str(visits))
+
+
+def create_visits_file_if_not_exists():
+    if os.path.isfile(VISITS_FILE_PATH):
+        print("Visits file already exists")
+        return
+
+    init_value = 0
+    with open(VISITS_FILE_PATH, "w+") as file:
+        file.write(str(init_value))
+
+    print(f"Visits file created with initial value {init_value}")
+
 @app.route("/")
 def home():
+    increment_visits()
     timezone = pytz.timezone(TIMEZONE)
     time = get_time(timezone)
     time_string = format_time(time, TIME_FORMAT)
@@ -26,4 +60,5 @@ def home():
 
 
 if __name__ == "__main__":
+    create_visits_file_if_not_exists()
     app.run(host=HOSTNAME, port=3000, debug=True)
