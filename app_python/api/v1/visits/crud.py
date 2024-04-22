2a07b8b827d463d528c0a1079b9778baa8fee18d
IlnurHA <Ilnur_ha@mail.ru>
2024-04-23 02:45:18 +0300

    Volume for visits

--- app_python/api/v1/visits/crud.py
+++ app_python/api/v1/visits/crud.py
@@ -1,16 +1,18 @@
 import os
 
+file = "./visits/visits.txt"
+
 
 def get_visits():
-    if not os.path.exists('visits'):
-        with open('visits', 'w') as f:
+    if not os.path.exists(file):
+        with open(file, 'w') as f:
             f.write(str(0))
         return 0
 
-    with open("visits", "r") as f:
+    with open(file, "r") as f:
         return int(f.read())
 
 
 def set_visits(new_visit: int):
-    with open("visits", "w") as f:
+    with open(file, "w") as f:
         f.write(str(new_visit))
