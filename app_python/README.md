b224e1088f2a46a205992813c4417e7ca30bcce6
metafates <vvz.dev@gmail.com>
2024-04-24 01:01:49 +0300

    lab12

--- app_python/README.md
+++ app_python/README.md
@@ -7,11 +7,12 @@ Consists of two parts
 1. Python API server that uses FastAPI
 2. Frontend client
 
-Server has two endpoints
+Server has three endpoints
 
 1. `/` for serving static files under [./static](./static) folder
 2. `/api/time?tz={tz}` route that accepts timezone and returns the time in this
    timezone in a format of `%Y-%m-%d %H:%M:%S`
+3. `/visits` for displaying total number of visits (api accesses)
 
 Client fetches the time on interval each second.
 
--- app_python/app.py
+++ app_python/app.py
@@ -1,10 +1,45 @@
-from datetime import datetime
+import os
+import os.path
 import pathlib
+import threading
+from datetime import datetime
 
 import pytz
 from fastapi import FastAPI, HTTPException
-from starlette.staticfiles import StaticFiles
 from prometheus_fastapi_instrumentator import Instrumentator
+from starlette.staticfiles import StaticFiles
+
+
+class Visits:
+    def __init__(self):
+        self.mutex = threading.Lock()
+        self.path = "data/visits"
+        self.visits = 0
+
+    def add(self):
+        self.mutex.acquire()
+
+        self.visits += 1
+        self.save()
+
+        self.mutex.release()
+
+    def load(self):
+        if not os.path.exists(self.path):
+            return
+
+        with open(self.path, "r") as f:
+            self.visits = int(f.read())
+
+    def save(self):
+        try:
+            os.mkdir("data")
+        except:
+            pass
+
+        with open(self.path, "w") as visits_file:
+            visits_file.write(str(self.visits))
+
 
 dir_path = pathlib.Path(__file__).parent.resolve()
 
@@ -24,12 +59,25 @@ async def _startup():
     instrumentator.expose(app)
 
 
+visits = Visits()
+visits.load()
+
+
 @api.get("/time")
 def get_time(tz: str):
+    visits.add()
+
     if tz not in pytz.all_timezones:
         raise HTTPException(status_code=400, detail=f"Unknown timezone {tz}")
 
     return {"time": get_time_with_tz(tz)}
+
+
+@api.get("/visits")
+def get_visits():
+    visits.add()
+
+    return {"visits": visits.visits}
 
 
 app.mount("/api", api)
