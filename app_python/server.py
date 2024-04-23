5e99a7a5aa984bb2c43333383fc896cbf17a9827
RamPrin <pak55256@gmail.com>
2024-04-23 13:11:07 +0300

    lab12

--- app_python/server.py
+++ app_python/server.py
@@ -1,11 +1,14 @@
+import os
 from datetime import datetime
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 import pytz
 
 server = FastAPI() # Create server
 
+templates = Jinja2Templates(directory='./')
 
 @server.get('/') 
 def root():
@@ -25,3 +28,21 @@ def time():
     return {
         'time': time.strftime("%H:%M:%S")  # Return current Moscow Time in format HH:MM:SS
     }
+
+def add_visit():
+    os.makedirs('data', exist_ok=True)
+    if not os.path.exists('data/visits'):
+        f = open('data/visits', 'w')
+        f.write('0')
+        f.flush()
+        f.close()
+    visits = int(open('data/visits').read())
+    visits += 1
+    with open('data/visits', 'w') as f:
+        f.write(str(visits))
+    return visits
+
+@server.get('/visits')
+def visits(request: Request):
+    visits = add_visit()
+    return templates.TemplateResponse(request=request, name='visit.html', context={'visits': visits})
