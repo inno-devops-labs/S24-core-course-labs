459a6e7b2d6553eb15e925cef8086a04a80c8c99
Ян Козыренко <kopn48@mail.ru>
2024-04-23 22:51:40 +0300

    lab12

--- app_python/app.py
+++ app_python/app.py
@@ -4,13 +4,35 @@ import pytz
 
 app = Flask(__name__)
 
+VOLUME="volume/visits.txt"
+
+def write_visits(visits):
+    with open(VOLUME, 'a') as f:
+        f.write(visits)
+        f.close()
+
+
+def get_visits():
+    with open(VOLUME, 'a') as f:
+        return int(f.read().strip())
+
+def incr():
+    vis = get_visits
+    write_visits(str(vis + 1))
 
 @app.route('/')
 def index():
     moscow_timezone = pytz.timezone('Europe/Moscow')
     moscow_time = datetime.now(moscow_timezone).strftime('%Y-%m-%d %H:%M:%S')
+
+    incr()
+    
     return render_template('index.html', moscow_time=moscow_time)
 
+@app.route("/visits")
+def visits():
+    visits = get_visits()
+    return f'Visits: {visits}\n'
 
 if __name__ == '__main__':
     app.run(debug=False)
