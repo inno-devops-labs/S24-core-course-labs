30ba1146e76766b85dbedc725260ca9dc25318b6
Nikita Fedorovich <n.fedorovich@innopolis.university>
2024-04-23 20:56:23 +0300

    fmt

--- app_python/app/app.py
+++ app_python/app/app.py
@@ -10,6 +10,7 @@ PORT = os.getenv('PORT')
 HOST = os.getenv('HOST')
 VISITS_FILE = os.getenv('FILE')
 
+
 def load_visits():
     try:
         with open(VISITS_FILE, 'r') as file:
@@ -17,9 +18,11 @@ def load_visits():
     except FileNotFoundError:
         return 0
 
+
 def save_visits(visits):
     with open(VISITS_FILE, 'w') as file:
         file.write(str(visits))
+
 
 @app.route('/')
 def get_time():
@@ -27,16 +30,19 @@ def get_time():
     current_time = datetime.now(moscow_tz).strftime('%Y-%m-%d %H:%M:%S')
     return f'The current time in Moscow is: {current_time}'
 
+
 @app.route('/visits')
 def get_visits():
     visits = load_visits()
     return jsonify({'visits': visits})
+
 
 @app.before_request
 def increment_visit():
     visits = load_visits()
     visits += 1
     save_visits(visits)
+
 
 if __name__ == '__main__':
     app.run(host=HOST, port=PORT, debug=True)
