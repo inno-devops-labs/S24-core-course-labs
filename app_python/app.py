b5a930c5887e26ef6f9741697360918ad1b0e422
pptx704 <rafeedm.bhuiyan@gmail.com>
2024-04-24 05:11:57 +0300

    Updated dependencies

--- app_python/app.py
+++ app_python/app.py
@@ -2,17 +2,22 @@
 Main application file. Contains the route information and necessary setups.
 """
 
+import os
+
 from flask import Flask, request
 from prometheus_flask_exporter import PrometheusMetrics
 
-from app_python.app_utils import return_time, increment_visits, return_visit_counts
-
-import os
+from app_python.app_utils import increment_visits, return_time, return_visit_counts
 
 app = Flask(__name__)
 metrics = PrometheusMetrics(app)
 
-@metrics.counter("requests_by_endpoint", "Number of requests by endpoint", labels={"endpoint": lambda: request.endpoint})
+
+@metrics.counter(
+    "requests_by_endpoint",
+    "Number of requests by endpoint",
+    labels={"endpoint": lambda: request.endpoint},
+)
 @app.route("/")
 def show_time():
     """
@@ -22,12 +27,14 @@ def show_time():
     timezone = os.getenv("APP_TIMEZONE", "Europe/Moscow")
     return return_time(timezone)
 
+
 @app.route("/metrics")
 def show_prometheus_metrics():
     """
     Metrics page. Shows the current time in Moscow in Prometheus format.
     """
     return return_time("Europe/Moscow", prometheus=True)
+
 
 @app.route("/visits")
 def show_visits():
--- app_python/app_utils.py
+++ app_python/app_utils.py
@@ -2,17 +2,25 @@
 Utility functions for the app. Can also be considered as the logic section.
 """
 
-from datetime import datetime
-import pytz
 import os
+from datetime import datetime
+
+import pytz
 
 
-def return_time(timezone: str = "Europe/Moscow") -> str:
+def return_time(timezone: str = "Europe/Moscow", prometheus=False) -> str:
     """
     Returns the current time in the given timezone.
     :param timezone: The timezone to get the current time from. (Optional, default: Europe/Moscow)
+    :param prometheus: Whether to return the time in Prometheus format. (Optional, default: False)
     :return: The current time in the given timezone.
     """
+    if prometheus:
+        return f"""
+        # HELP current_time_in_{timezone} The current time in {timezone}.
+        # TYPE current_time_in_{timezone} gauge
+        current_time_in_{timezone} {return_tz_time(timezone)}
+        """
     return f"""
     <h1>Current time in {timezone}: {return_tz_time(timezone)}</h1>
     """
@@ -26,29 +34,45 @@ def return_tz_time(timezone) -> str:
     """
     return datetime.now(pytz.timezone(timezone)).strftime("%H:%M (%I:%M %p)")
 
+
 def get_visits() -> int:
+    """
+    Gets the number of visits from the file.
+    :return: The number of visits.
+    """
+
     filename = os.getenv("VISITS_FILE")
     if not filename:
         raise ValueError("The VISITS_FILE environment variable is not set.")
     # if the file does not exist, create it
     if not os.path.exists(filename):
-        with open(filename, "w") as f:
+        with open(filename, "w", encoding="utf-8") as f:
             f.write("0")
         return 0
 
     # read the current number of visits
-    with open(filename, "r") as f:
+    with open(filename, "r", encoding="utf-8") as f:
         visits = int(f.read())
         return visits
-    
+
+
 def increment_visits():
+    """
+    Increments the number of visits by 1.
+    """
+
     visits = get_visits()
     visits += 1
     filename = os.getenv("VISITS_FILE")
-    with open(filename, "w") as f:
+    with open(filename, "w", encoding="utf-8") as f:
         f.write(str(visits))
 
+
 def return_visit_counts() -> str:
+    """
+    Returns the number of visits to the page.
+    :return: The number of visits.
+    """
     visits = get_visits()
     return f"""
     <h1>Number of visits: {visits}</h1>
