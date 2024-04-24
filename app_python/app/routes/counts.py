3bf2885d65ace42b54ca9f3c00d840e80b90c9ce
Tanmay Sharma <sharmatanmay617@gmail.com>
2024-04-24 04:59:03 +0300

    Fix Bug

--- app_python/app/routes/counts.py
+++ app_python/app/routes/counts.py
@@ -5,27 +5,27 @@ import pytz
 from pathlib import Path
 
 app_folder = Path(os.path.dirname(__file__))
-visits_file = os.path.join(app_folder.parent, "visits_folder", "visits.txt")
+visits_file_path = os.path.join(app_folder.parent, "visits_folder", "visits.txt")
 
 count_blueprint = Blueprint('count', __name__)
 
 def _increment_visits() -> None:
-    with open(visits_file, "r") as visits_file:
+    with open(visits_file_path, "r") as visits_file:
         visits = int(visits_file.readline())
-    with open(visits_file, "w") as visits_file:
+    with open(visits_file_path, "w") as visits_file:
         visits_file.write(str(visits + 1))
 
 @count_blueprint.get("/visits")
 def get_visits():
     _increment_visits()
 
-    with open(visits_file) as visits_file:
+    with open(visits_file_path) as visits_file:
         visits = int(visits_file.readline())
 
     return visits
 
 def _create_visit():
     init_value = 0
-    if not os.path.isfile(visits_file):
-        with open(visits_file, "w+") as file:
+    if not os.path.isfile(visits_file_path):
+        with open(visits_file_path, "w+") as file:
             file.write(str(init_value))
