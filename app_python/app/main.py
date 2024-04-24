6beeaf75c9af69ce29d7168dacd20b3a927e0862
Albert Avkhadeev <albertavkhadeev@gmail.com>
2024-04-24 07:57:54 +0300

    Sol

--- app_python/app/main.py
+++ app_python/app/main.py
@@ -7,11 +7,23 @@ import pytz
 app = FastAPI()
 
 moscow_time_zone = pytz.timezone("Europe/Moscow")
+visits_file = "visits.txt"
 
 instrumentator = Instrumentator().instrument(app)
 
+try:
+    with open(visits_file, "r") as f:
+        visits_count = int(f.read())
+except FileNotFoundError:
+    visits_count = 0
+
+
 @app.get("/")
 async def root() -> FileResponse:
+    global visits_count
+    visits_count += 1
+    with open(visits_file, "w") as f:
+        f.write(str(visits_count))
     return FileResponse('view/index.html')
 
 
@@ -23,3 +35,7 @@ async def on_startup():
 @app.get("/api/time")
 async def get_time() -> dict:
     return {'time': datetime.now(tz=moscow_time_zone).strftime("%H:%M:%S")}
+
+@app.get("/visits")
+async def get_visits() -> dict:
+    return {'visits': visits_count}
