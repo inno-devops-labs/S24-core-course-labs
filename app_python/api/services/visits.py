713d3d84abade63e80269904b9d50c245bbdb8f9
kamil <almetov.kamil@gmail.com>
2024-05-01 11:48:23 +0300

    fix

--- app_python/api/services/visits.py
+++ app_python/api/services/visits.py
@@ -1,11 +1,11 @@
 def increment_visits():
     visits_n = get_visits()
-    with open("visits.txt", "w") as visits:
+    with open("visits/visits.txt", "w") as visits:
         visits.write(str(visits_n + 1))
 
 
 def get_visits():
-    with open("visits.txt", "r") as visits:
+    with open("visits/visits.txt", "r") as visits:
         s = visits.read()
         if s == "":
             return 0
