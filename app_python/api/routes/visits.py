1fb1e0a0908549fd62fb71ae4f8b5f6b994fe227
kamil <almetov.kamil@gmail.com>
2024-05-01 11:32:13 +0300

    fix visits

--- app_python/api/routes/visits.py
+++ app_python/api/routes/visits.py
@@ -3,8 +3,9 @@ from flask import Blueprint, render_template
 from api.services.visits import get_visits
 
 
-visits = Blueprint('/visits', __name__)
+visits = Blueprint("/visits", __name__)
 
-@visits.route('/visits')
+
+@visits.route("/visits")
 def get_visits_endpoint():
     return render_template("visits.html", visits=get_visits())
