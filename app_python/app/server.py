366ed1a4f21fbdbcd7ec86ef7b26c3987ff119da
Maxim Insapov <m.insapov@innopolis.university>
2024-05-07 11:18:36 +0300

    lab14

--- app_python/app/server.py
+++ app_python/app/server.py
@@ -7,12 +7,28 @@ from textwrap import dedent
 from zoneinfo import ZoneInfo
 
 from ntplib import NTPClient
-from sanic import Sanic
+from sanic import Sanic, response
 from sanic_ext import render
+import prometheus_client as prometheus
 
 from config import AppConfig
 
+request_counter = prometheus.Counter("sanic_requests_total",
+                                     "Track the total number of requests",
+                                     ["method", "endpoint"])
+
+request_time = prometheus.Summary("sanic_request_time",
+                                  "Track the time",
+                                  ["method", "endpoint"])
+
 app = Sanic("MoscowTime", config=AppConfig())
+
+@app.middleware('request')
+async def track_requests(request):
+    # Increase the value for each request
+    # pylint: disable=E1101
+    request_counter.labels(method=request.method,
+                           endpoint=request.path).inc()
 
 
 @app.before_server_start
@@ -45,6 +61,9 @@ async def index_handle(request):
 
     Requests current time from Google NTP server and returns a template with it
     """
+
+    start_time = datetime.now()
+
     template = dedent(
         """
         <!DOCTYPE html>
@@ -66,6 +85,9 @@ async def index_handle(request):
         version=request.app.config.NTP_VERSION)
     utc_time = datetime.fromtimestamp(response.tx_time, tz=timezone.utc)
     moscow_time = datetime.astimezone(utc_time, tz=ZoneInfo("Europe/Moscow"))
+
+    end_time = datetime.now()
+    request_time.labels(method=request.method, endpoint=request.path).observe(end_time.timestamp() - start_time.timestamp())
 
     return await render(
         template_source=template,
@@ -98,6 +120,13 @@ async def visits_handle(request):
         context={'visits': visits_count},
         app=app,
     )
+
+@app.get("/metrics")
+async def metrics(request):
+    output = prometheus.exposition.generate_latest().decode("utf-8")
+    content_type = prometheus.exposition.CONTENT_TYPE_LATEST
+    return response.text(body=output,
+                         content_type=content_type)
 
 
 if __name__ == '__main__':
