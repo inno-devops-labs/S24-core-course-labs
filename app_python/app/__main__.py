7957e272fd37880ead686af457689917746f781b
MinusOne-1 <lannaig03@yandex.ru>
2024-05-14 08:13:29 +0300

    lab8(task 1 and a small changes to the app_python to let prometheus collect metrics)

--- app_python/app/__main__.py
+++ app_python/app/__main__.py
@@ -3,13 +3,18 @@ Routing of the app
 """
 
 from flask import Flask, render_template
-
-from .utils import get_moscow_time
+from prometheus_flask_exporter import PrometheusMetrics
+from utils import get_moscow_time
 
 app = Flask(__name__)
+metrics = PrometheusMetrics(app, group_by='endpoint')
 
 
 @app.route('/')
+@metrics.counter(
+    'cnt_collection', 'Number of invocations per collection', labels={
+        'status': lambda resp: resp.status_code
+    })
 def index():
     """Render funtion for start page of the app"""
     moscow_time = get_moscow_time()
