2d5841a45d41c8d7a96ad298793c226d18529e21
Wesam-Naseer <102805195+Wesam-Naseer@users.noreply.github.com>
2024-08-31 23:20:16 +0300

    lab 08

--- app_python/app.py
+++ app_python/app.py
@@ -1,15 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, Response
 from datetime import datetime, timezone, timedelta
+from prometheus_client import generate_latest
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def show_time():
-    moscow_time = datetime.utcnow() + timedelta(hours=3)
+    moscow_time = datetime.now() + timedelta(hours=3)
     formatted_time = moscow_time.strftime('%Y-%m-%d %H:%M:%S')
     return formatted_time
 
+@app.route('/metrics')
+def metrics():
+    return Response(generate_latest(), mimetype="text/plain")
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
