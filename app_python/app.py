24bd2ab0e99c6df9e96a78e361d5b024409efa32
Aksinya <88349390+aximaxxi@users.noreply.github.com>
2024-11-09 01:36:51 +0300

    lab3

--- app_python/app.py
+++ app_python/app.py
@@ -18,3 +18,4 @@ def display_time():
 
 if __name__ == '__main__':
     app.run(debug=True)
+    
--- app_python/test_app.py
+++ app_python/test_app.py
@@ -3,15 +3,15 @@ from flask_testing import TestCase
 from datetime import datetime, timezone, timedelta
 from app import app
 
+
 class TestApp(TestCase):
-    
     def create_app(self):
         app.config['TESTING'] = True
         return app
 
     def test_current_time_displayed(self):
         response = self.client.get('/')
-        self.assert200(response) 
+        self.assert200(response)
         self.assert_template_used('index.html')
 
         moscow_time = datetime.now(timezone(timedelta(hours=3)))
